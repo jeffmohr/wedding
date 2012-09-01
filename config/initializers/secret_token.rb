d7d9cd7612ac27ac35479a106b62d2e6c2d31d1b
Jeff Mohr <jeff@kumupowered.com>
2012-09-01 11:57:43 -0700

    Initial commit of basic rails app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = '1745892c7235fe4f564f215eb99df530da198e39bd66a8e6fc26702e40a59175fed62026eef46a4f2a61f2bed0bd3fb5e0faf336c5a867849ad0c0c0f3306efa'
